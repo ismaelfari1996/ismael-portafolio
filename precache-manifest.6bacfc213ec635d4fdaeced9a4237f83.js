88c3ffdbc568105c23beee75351b58b59c5b9010
ismaelfari1996 <abel_fari@hotmail.com>
2023-01-16 15:11:52 -0500

    Updates

---
+++ precache-manifest.6bacfc213ec635d4fdaeced9a4237f83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cc5fa1cccd7f63868f2",
+    "url": "/ismael-portafolio/static/css/main.32dd99c7.chunk.css"
+  },
+  {
+    "revision": "0cc5fa1cccd7f63868f2",
+    "url": "/ismael-portafolio/static/js/main.0cc5fa1c.chunk.js"
+  },
+  {
+    "revision": "700e8f635bfe5bb56b19",
+    "url": "/ismael-portafolio/static/js/1.700e8f63.chunk.js"
+  },
+  {
+    "revision": "a7918f865240b9804edb",
+    "url": "/ismael-portafolio/static/js/runtime~main.a7918f86.js"
+  },
+  {
+    "revision": "72cfdf54fb59cad47a3bb7006c8456cc",
+    "url": "/ismael-portafolio/static/media/ismael.72cfdf54.jpg"
+  },
+  {
+    "revision": "ba037cf23731527324c0bddee8027510",
+    "url": "/ismael-portafolio/static/media/loadPageImage.ba037cf2.jpg"
+  },
+  {
+    "revision": "892502fe997e9529d146d4b1b4654704",
+    "url": "/ismael-portafolio/index.html"
+  }
+];

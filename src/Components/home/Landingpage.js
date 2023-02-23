443067fa9b1a4beaa4fd5b4fc25166d4e6efa8ff
ismaelfari1996 <abel_fari@hotmail.com>
2023-02-23 17:11:34 -0500

    commint

--- src/Components/home/Landingpage.js
+++ src/Components/home/Landingpage.js
@@ -1,19 +1,48 @@
-import React from "react"
-import LoadImg from "../../assets/loadPageImage.jpg";
+import React from "react";
+import { useState, useEffect } from "react";
+import LoadImg from "../../assets/loadPageImage1.png";
 export default function Landingpage() {
+
+    const typed = ({ text }) => {
+        const [currentText, setCurrentText] = useState('');
+        let index = 0;
+      
+        useEffect(() => {
+          let interval;
+          if (index < text.length) {
+            interval = setInterval(() => {
+              setCurrentText(text.slice(0, index));
+              index++;
+              if(index==text.length){
+                index=0;
+              }
+            }, 150);
+          } else {
+            clearInterval(interval);
+          }
+          return () => clearInterval(interval);
+        }, [text, index]);
+
+        
+        return <span>{currentText}</span>;
+    }
+
     return (
-        <div className="container mt-0 min-vh-100">
+        <div className="container mt-0 min-vh-100 " >
             <div className="row d-flex">
-                <div className="col-md-6 col-sm-6 align-items-center introduction-text">
-                    <p>Hola, soy Ismael Farinango</p>
-                    <h1>Ingeniero en Sistemas</h1>
-                    <p>Desarrolladro de software, apasionado por la tecnologia y la programacion</p>
+                <div className="col-md-6 col-sm-6 align-items-center introduction-text d-block ">
+                    <p>Hola, mi nombre es</p>
+                    <h1>Ismael Farinango.</h1>
+                    <h1 style={{"color":"#a6a6a6"}}>Desarrollador de Software.</h1>
+                    <p style={{"color":"#a6a6a6"}}>Soy Ingeniero en Sistemas, me apasionado la tecnología y la programación, 
+                      siempre estoy abierto a nuevas propuestas y retos dentro de la industria.</p>
                     <button className="btn btn-primary mt-5">Trabajemos juntos</button>
                 </div>
-                <div className="col-md-6 col-sm-6  d-flex align-items-center py-5 d-none landig-img">
-                    <img src={LoadImg} alt="Imagen" className="img-fluid" />
+                <div className="col-md-6 col-sm-6  d-flex align-items-center py-5 d-none d-sm-block ">
+                    <img src={LoadImg} alt="Imagen" className="img-fluid py-4" />
                 </div>
             </div>
+            
         </div>
 
     )
--- src/Components/home/projects.js
+++ src/Components/home/projects.js
@@ -3,14 +3,15 @@ import React, { useEffect, useState } from "react";
 import Config from "../../config/config.json";
 export default function Project() {
     const [gitHubProjects, setGitHubprojects] = new useState([]);
+    //Consume la API de Github
     useEffect(() => {
-        axios.get(Config.git)
+        axios.get(Config.git) //La url se encuentra configurado en config.json
             .then(value => setGitHubprojects(value.data))
             .catch((error) => {
                 console.log("ERROR");
             })
     }, []);
-    console.log(gitHubProjects);
+
     return (
         <div className="container">
             <div>
@@ -18,38 +19,94 @@ export default function Project() {
             </div>
             <div className="row">
                 {
-                    Object.values(gitHubProjects).map((project, index) => {
+                    //Carga de los proyectos externos, mismo que se encuentran configurados en config.json
+                    Object.values(Config.projects).map((project, index) => {
                         return (
-                            <div class="col-md-6">
-                                <div class="card shadow-lg p-3 mb-5 bg-white rounded card">
-                                    <div class="card-body">
-                                        <h5 class="card-title"> {project.name}</h5>
-                                        <p class="card-text">{project.description}</p>
-                                        <div className="row">
-                                            <div className="col-md-6">
-                                        <a href={project.html_url} class="btn btn-outline-primary d-flex mx-auto align-items-center" target={'_blank'}>
-                                            <i class="lab la-github"></i> Repositorio</a>
-                                            </div>
-                                            <div className="col-md-6">
-                                        <a href={project.svn_url + "/archive/master.zip"} class="btn btn-outline-primary d-flex mx-auto align-items-center text-center" target="_blank">
-                                            <i class="lab la-git-alt"></i> Clonar</a>
-                                            </div>
+                            <div key={index} className="col-md-6" data-aos="fade-right">
+                                <div className="card shadow-lg p-3 mb-5 bg-white rounded card" style={{ "minHeight": "320px" }}>
+                                    <div className="card-body">
+                                        <div className="row" style={{ "minHeight": "150px" }}>
+                                            <h5 className="card-title"> {project.name}</h5>
+                                            <p className="card-text">{project.description}</p>
                                         </div>
+                                        <hr/>
+                                        <div className="row mt-5">
+                                            <div className="col-md-12">
+                                                <a href={project.url} className="btn btn-outline-primary d-flex mx-auto align-items-center" target={'_blank'}>
+                                                    <i className="las la-external-link-alt"></i> Visitar</a>
+                                            </div>
 
-                                        <hr />
-                                        <div class="pb-3">Lenguajes: <a class="badge badge-light card-link" href="#">PHP: 100 %</a>
                                         </div>
-                                        <p class="card-text">
-                                            <small class="text-muted">1 hour ago</small>
-                                        </p>
                                     </div>
                                 </div>
                             </div>
 
                         )
                     })
+
+                }
+                {
+                    // Carga los proyectos que se encuentran en GitHub
+                    Object.values(gitHubProjects).map((project, index) => {
+                        return (
+                            project.name === "ismaelfari1996" ? "" :
+                                <div key={index} className="col-md-6" data-aos="fade-right">
+                                    <div className="card shadow-lg p-3 mb-5 bg-white rounded card" style={{ "minHeight": "320px" }}>
+                                        <div className="card-body">
+                                            <div className="row" style={{ "minHeight": "150px" }}>
+                                                <h5 className="card-title"> {project.name}</h5>
+                                                <p className="card-text">{project.description}</p>
+                                            </div>
+                                            <hr/>
+                                            <Languages url={project.languages_url} />
+                                            
+                                            <div className="row ">
+                                                <div className="col-md-6">
+                                                    <a href={project.html_url} className="btn btn-outline-primary d-flex mx-auto align-items-center" target={'_blank'}>
+                                                        <i className="lab la-github icon-git"></i>  Repositorio</a>
+                                                </div>
+                                                <div className="col-md-6 space-mg">
+                                                    <a href={project.svn_url + "/archive/master.zip"} className="btn btn-outline-primary d-flex mx-auto align-items-center text-center" target="_blank">
+                                                        <i className="las la-cloud-download-alt icon-git"></i>  Clonar</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                        )
+                    })
                 }
             </div>
         </div>
+    )
+}
+
+const Languages = ({ url }) => {
+    const [lenguage, setLenguege] = new useState();
+    const lenguageValue = [];
+    let total = 0;
+    useEffect(() => {
+        axios.get(url)
+            .then(value => setLenguege(value.data))
+            .catch(error => { throw error })
+    }, [])
+
+    for (let i in lenguage) {
+        lenguageValue.push(i);
+        total += lenguage[i];
+    }
+    return (
+        lenguageValue.length ?
+            <div className="mb-2">Lenguajes: {
+                lenguageValue.map(value => {
+                    return (
+                        <>
+                            <strong><small>{value+": "}</small></strong>
+                            <small>{Math.trunc(((lenguage[value] / total)) * 1000) / 10 + "%  "}</small>
+                        </>
+                    )
+                })}
+            </div> : ""
     )
 }

b7ecf8189ea62118ae1c664ede8fc4caec41affb
ismaelfari1996 <abel_fari@hotmail.com>
2023-03-04 10:29:43 -0500

    change logo name

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
         <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
             <div className="container">
                 <img src={Logo} className="img img-fluid" style={{"width":"30px","height":"30px"}} alt="ismael-logo" />
-                <a className="navbar-brand mx-2" href="#home">{"Isamel"}</a>
+                <a className="navbar-brand mx-2" href="#home">{"Ismael"}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>

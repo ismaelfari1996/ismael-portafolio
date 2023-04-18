1c038b97f8ca1de38584d8f7d797012ef9f6ed19
ismaelfari1996 <abel_fari@hotmail.com>
2023-04-18 18:23:15 -0500

    update

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,6 +4,7 @@ export default function Footer(){
     return(
         <div className="container-fluid p-3 border d-flex bg-light">
             <label className="d-flex mx-auto">Ismael Farinango - 2023</label>
+            <small><a href="https://icons8.com/icons/" target={"_blank"}  className="me-5 text-decoration-none text-muted">Icons by Icons8</a></small>
         </div>
     )
 }

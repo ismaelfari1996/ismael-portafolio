3decfc11141b2644eb0d1cfb5581f23559001893
ismaelfari1996 <abel_fari@hotmail.com>
2024-05-17 19:01:54 -0500

    whatsapp component

--- src/Components/watsapp/whatsapp.jsx
+++ src/Components/watsapp/whatsapp.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Wsp() {
     return (
         <a className="btn-wsp" href="https://api.whatsapp.com/send?phone=593999545984" target={"_blank"}
-        rel="noopener noreferrer">
+        rel="noopener noreferrer" aria-label="Whatssap">
             <i className="lab la-whatsapp"></i>
         </a>
     );
